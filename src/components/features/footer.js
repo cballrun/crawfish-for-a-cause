9aa4ea3ec6c4414a0d23655d8d6f8b3eb8ab495a
cballrun <carter.k.ball@gmail.com>
2023-07-13 16:44:45 -0400

    Footer text styling and positioning

--- src/components/features/footer.js
+++ src/components/features/footer.js
@@ -3,7 +3,15 @@ import styles from '../styles/footer.module.css'
 function Footer() {
     return (
         <div className={styles.footer}>
-
+            <text className={styles.footerText}>
+                Good Food. 
+            </text>
+            <text className={styles.footerTextMid}>
+               Good Music.
+            </text>
+            <text className={styles.footerText}>
+               Good Fun.
+            </text>
         </div>
     )
 }
