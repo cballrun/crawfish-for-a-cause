eeb0ac2b0317645c63938eb74ad13bf5527872a8
cballrun <carter.k.ball@gmail.com>
2023-07-18 01:13:18 -0400

    Create and style header text

--- src/components/features/header.js
+++ src/components/features/header.js
@@ -3,14 +3,22 @@ import NavBar from './navBar'
 import Logo from './logo'
 
 function Header() {
+    const bottomWords = "FOR A CAUSE";
+    const words = bottomWords.split(" ");
+    
     return (
         <div className={styles.header}>
-
-                <Logo/>
-                <NavBar/>
-
+          <Logo />
+          <NavBar />
+          <div className={styles.headerText}>
+            <span className={styles.topWord}>CRAWFISH</span>
+            <span className={styles.bottomWords}>
+              {words.slice(0, -1).join(" ")}{" "}
+              <span className={styles.lastWord}>{words[words.length - 1]}</span>
+            </span>
+          </div>
         </div>
-    )
-}
+      );
+    }
 
 export default Header
